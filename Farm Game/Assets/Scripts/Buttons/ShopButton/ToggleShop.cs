98e1ac3ee8d15d97f4a8c64bc4604960b7374ad2
Henry <sode787@yahoo.com>
2019-02-09 21:14:21 -0600

    Toggle shop button

---
+++ Farm Game/Assets/Scripts/Buttons/ShopButton/ToggleShop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleShop : MonoBehaviour
+{
+	private GameObject shopButton;
+	private bool isShowing = false;
+
+	void Start()
+	{
+		shopButton = GameObject.Find("ShopCanvas");
+	}
+
+  //   // Update is called once per frame
+  //   void Update()
+  //   {
+		// if (Input.GetKeyDown("escape"))
+		// {
+		// 	isShowing = !isShowing;
+		// 	shopButton.SetActive(false) ;
+		// }   
+  //   }
+
+    public void ToggleShopFunction()
+    {
+    	isShowing = !isShowing;
+		shopButton.SetActive(isShowing) ;
+    }
+}
--- Farm Game/Assets/Scripts/Shop/PlantDisplay.cs
+++ Farm Game/Assets/Scripts/Shop/PlantDisplay.cs
@@ -10,27 +10,26 @@ using UnityEngine.UI;
 */
 public class PlantDisplay : MonoBehaviour
 {
-	// Takes in a Scriptable Object 
+	// Takes in a Scriptable Object called plant
 	public Plant plant;
 
-	public Text nameText;
-	public Text descriptionText;
+	// public Text nameText;
+	// public Text descriptionText;
 
 	public Image artworkImage;
 
-	public Text requiredLevelText;
+/*	public Text requiredLevelText;
 	public Text goldText;
-	public Text harvestTime;
+	public Text harvestTime;*/
 
 	void Start()
 	{
-		nameText.text = plant.plantName;
-		descriptionText.text = plant.description;
+		// nameText.text = plant.plantName;
+		// descriptionText.text = plant.description;
 
 		artworkImage.sprite = plant.artwork;
-		requiredLevelText.text = plant.requriedLevel.ToString();
-		goldText.text = plant.goldCost.ToString();
-		harvestTime.text = plant.harvestTime.ToString();
-
+		// requiredLevelText.text = plant.requriedLevel.ToString();
+		// goldText.text = plant.goldCost.ToString();
+		// harvestTime.text = plant.harvestTime.ToString();
 	}
 }
