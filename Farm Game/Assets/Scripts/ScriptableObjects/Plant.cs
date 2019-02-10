68845fe8da4beafc363aacf4c14887fcfcce5ab8
Henry <sode787@yahoo.com>
2019-02-09 19:51:50 -0600

    Implemented Scriptable Objects for plants and basic layout of shop

---
+++ Farm Game/Assets/Scripts/ScriptableObjects/Plant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Plant", menuName = "Plant")]
+public class Plant : ScriptableObject
+{
+	public string plantName;
+	public string description;
+
+	// required level to buy/plant?
+	public int requriedLevel;
+	public Sprite artwork;
+
+	public int goldCost;
+	[Header("Seconds")]
+	public double harvestTime;
+	// water time should be half of harvestTime
+	
+
+
+}
---
+++ Farm Game/Assets/Scripts/Shop/PlantDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*
+	This templete is used with Scriptable Obejects created 
+	in the Prefab/Plant/* folder
+*/
+public class PlantDisplay : MonoBehaviour
+{
+	// Takes in a Scriptable Object 
+	public Plant plant;
+
+	public Text nameText;
+	public Text descriptionText;
+
+	public Image artworkImage;
+
+	public Text requiredLevelText;
+	public Text goldText;
+	public Text harvestTime;
+
+	void Start()
+	{
+		nameText.text = plant.plantName;
+		descriptionText.text = plant.description;
+
+		artworkImage.sprite = plant.artwork;
+		requiredLevelText.text = plant.requriedLevel.ToString();
+		goldText.text = plant.goldCost.ToString();
+		harvestTime.text = plant.harvestTime.ToString();
+
+	}
+}
