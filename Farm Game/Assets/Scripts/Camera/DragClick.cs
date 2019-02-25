e467b34226e7f7b8d9a88237b91e590e2a5f7581
Donny Pham <Donny1Pham@yahoo.com>
2019-02-25 13:31:48 -0600

    v[1.0.1.1] - Housekeeping files and art
    -Added a new grass tile
    -Fixed a light bug
    -Rearranged some files for ease

--- Farm Game/Assets/Scripts/Camera/DragClick.cs
+++ Farm Game/Assets/Scripts/Camera/DragClick.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_DragClick : MonoBehaviour {
+public class DragClick : MonoBehaviour {
 
 	private Vector3 ResetCamera;
 	private Vector3 Origin;
