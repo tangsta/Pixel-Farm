be74e2abcdbb2c70f36de9d4fe69f64219512bc7
Donny Pham <Donny1Pham@yahoo.com>
2019-02-28 13:30:08 -0600

    v[1.0.1.4] - Plot/Tile Scripts
    - Added a new Tile Asset
    - Script to initialize tiles in tilemap
    - Added plotbase which contains tile and related data
    - Added ability to interact with tiles in game

--- Farm Game/Assets/Scripts/Camera/DragClick.cs
+++ Farm Game/Assets/Scripts/Camera/DragClick.cs
@@ -7,27 +7,27 @@ public class DragClick : MonoBehaviour {
 	private Vector3 ResetCamera;
 	private Vector3 Origin;
 	private Vector3 Diference;
-	private bool Drag=false;
+	private bool Drag = false;
 
 	void Start () {
 		ResetCamera = Camera.main.transform.position;
 	}
 	void LateUpdate () {
 		if (Input.GetMouseButton (0)) {
-			Diference=(Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
-			if (Drag==false){
-				Drag=true;
-				Origin=Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Diference = (Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
+			if (Drag == false){
+				Drag = true;
+				Origin = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			}
 		} else {
-			Drag=false;
+			Drag = false;
 		}
-		if (Drag==true){
+		if (Drag == true){
 			Camera.main.transform.position = Origin-Diference;
 		}
 		//RESET CAMERA TO STARTING POSITION WITH RIGHT CLICK
 		if (Input.GetMouseButton (1)) {
-			Camera.main.transform.position=ResetCamera;
+			Camera.main.transform.position = ResetCamera;
 		}
 	}
 }
---
+++ Farm Game/Assets/Scripts/Tilemap/TileClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class TileClick : MonoBehaviour
+{
+    private PlotBase tile;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 PointClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int worldPoint = new Vector3Int(Mathf.FloorToInt(PointClick.x),
+                Mathf.FloorToInt(PointClick.y), 0);
+
+            var tiles = PlotManager.instance.tiles;
+
+            if(tiles.TryGetValue(worldPoint, out tile))
+            {
+                Debug.Log(tile.Name);
+                tile.Tilemap.SetTileFlags(tile.LocalPlace, TileFlags.None);
+                tile.Tilemap.SetColor(tile.LocalPlace, Color.blue);
+            }
+        }
+    }
+}
