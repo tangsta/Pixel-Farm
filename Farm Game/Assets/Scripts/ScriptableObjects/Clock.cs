2a995b2b9b9d2fe1902c878fb131ad4ff7c30987
Donny Pham <Donny1Pham@yahoo.com>
2019-03-26 17:09:38 -0500

    v[1.0.7.2] - Basic Clock Implemented
    - Trying to test out crop and grounds implementation according to the tilemaps.

---
+++ Farm Game/Assets/Scripts/ScriptableObjects/Clock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public CropMap CMap; 
+
+    public void Start()
+    {
+        StartCoroutine(LoopFunction(1));
+    }
+
+    private IEnumerator LoopFunction(float waitTime)
+    {
+        while (true)
+        {
+            Debug.Log("print.");
+            CMap.GrowAll();
+            CMap.TriggerCropEffects();
+
+            yield return new WaitForSeconds(waitTime);
+            //Second Log show passed waitTime (waitTime is float type value ) 
+            Debug.Log("print1.");
+        }
+    }
+}
