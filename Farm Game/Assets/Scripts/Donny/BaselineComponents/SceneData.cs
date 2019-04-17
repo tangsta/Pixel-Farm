345e77a7e06f2db0c9c9804e2cef48636378f1ab
Donny Pham <Donny1Pham@yahoo.com>
2019-04-17 17:10:45 -0500

    v[1.0.9.5] merge fixes

--- Farm Game/Assets/Scripts/Donny/BaselineComponents/SceneData.cs
+++ Farm Game/Assets/Scripts/Donny/BaselineComponents/SceneData.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD:Farm Game/Assets/Scripts/BaselineComponents/SceneData.cs
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 
 //Unites and globalizes values from Tilemaps
@@ -10,19 +9,4 @@ public class SceneData : ScriptableObject
     public Dictionary<Vector3Int, Land> Grounds;
     public Dictionary<Vector3Int, CropStats> Crops;
     //public Dictionary<Vector3, bool> Grasses;
-
-=======
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-//Unites and globalizes values from Tilemaps
-[CreateAssetMenu]
-public class SceneData : ScriptableObject
-{
-    public Dimensions Dimensions;
-    public Dictionary<Vector3Int, GroundStats> Grounds;
-    public Dictionary<Vector3Int, CropStats> Crops;
-    //public Dictionary<Vector3, bool> Grasses;
-
->>>>>>> PlantLayer:Farm Game/Assets/Scripts/Donny/BaselineComponents/SceneData.cs
 }
